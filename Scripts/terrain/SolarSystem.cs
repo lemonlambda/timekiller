771d345583fb081b6fa28e7ee863070b3abcbd09
Lemon <lemonjameslem@protonmail.com>
2025-02-22 17:27:36 -0500

    General making the game look better

--- Scripts/terrain/SolarSystem.cs
+++ Scripts/terrain/SolarSystem.cs
@@ -1,16 +1,19 @@
 using Godot;
 using System;
 
+using Timekiller.Helpers;
 using Timekiller.StateManager;
 
 namespace Timekiller.Terrain {
 	public partial class SolarSystem : GodotObject {
 		public int GID;
+		public string Name;
 		public Planet[] Planets;
 
 		public SolarSystem() {
 			this.GID = Manager.GetNewGID();
-			this.Planets = new Planets[0];
+			this.Name = UniqueName.GenerateUniqueSystemName();
+			this.Planets = new Planet[0];
 		}
 	}
 }
