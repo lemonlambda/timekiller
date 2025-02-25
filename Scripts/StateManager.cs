06c7ea971e75db9a5c43b6e79408ea910c63390a
Lemon <lemonjameslem@protonmail.com>
2025-02-25 10:58:00 -0500

    Added user

--- Scripts/StateManager.cs
+++ Scripts/StateManager.cs
@@ -7,11 +7,18 @@ using Timekiller.Terrain;
 namespace Timekiller.StateManager {
 	public static class Manager {
 		public static SolarSystem[] Systems = Enumerable.Range(0, 100).Select(id => id == 0 ? new SolarSystem("Zavka", (int?)3) : new SolarSystem()).ToArray();
+		public static User Player = new User(Systems[0].GID, 0, 1, 0, 0);
 	
 		private static int gid = 0;
 
 		public static int GetNewGID() {
 			return gid++;
+		}
+
+		public static void Tick() {
+			foreach (SolarSystem system in Systems) {
+				system.Tick();
+			}
 		}
 	}
 }
